038cd3cd457834f19b8d444b5b57979c524819bc
Ryan Park <parkryan844@gmail.com>
2025-10-24 19:17:18 -0700

    Finished source():string of tweet.ts

--- ts/tweet.ts
+++ ts/tweet.ts
@@ -9,8 +9,34 @@ class Tweet {
 
 	//returns either 'live_event', 'achievement', 'completed_event', or 'miscellaneous'
     get source():string {
-        //TODO: identify whether the source is a live event, an achievement, a completed event, or miscellaneous.
-        return "unknown";
+        const s = this.text.toLowerCase();
+
+        if (
+            s.startsWith("just completed ") ||
+            s.startsWith("completed a ") ||
+            / completed (a|an) [\d\.,]+ (mi|mile|miles|km|kilometer|kilometers) /.test(s)
+        ) {
+            return "completed_event";
+        }
+
+        if (
+            s.includes("watch my run right now") ||
+            ((s.startsWith("i'm") || s.startsWith("im ")) && s.includes(" with @runkeeper")) ||
+            (s.startsWith("just posted a ") && s.includes(" with @runkeeper"))
+        ) {
+            return "live_event";
+        }
+
+        if (
+            s.includes("personal record") ||
+            s.startsWith("achieved ") ||
+            s.includes("i just set a goal") ||
+            s.includes("set a new goal")
+        ) {
+            return "achievement";
+        }
+        
+        return "miscellaneous";
     }
 
     //returns a boolean, whether the text includes any content written by the person tweeting.
